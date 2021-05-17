4fe2dcf93b470f41f48b80d07d688b33a41462d2
Michael Gailling <michael.gailling@gmail.com>
2021-05-17 14:55:31 -0400

    GCTabView -Significantly simplified functionality

--- GC_Views/GCTabView.py
+++ GC_Views/GCTabView.py
@@ -43,6 +43,10 @@ class GCTabView(QFrame):
         self.tw_tabWidget.insertTab(1, self.tv_tableView, "Asset Selection")
         self.tw_tabWidget.insertTab(2, self.rv_resultsView, "Preview")
 
+        self.tw_tabWidget.setTabEnabled(0, True)
+        self.tw_tabWidget.setTabEnabled(1, False)
+        self.tw_tabWidget.setTabEnabled(2, False)
+
         self.hbl_tab_layout.addWidget(self.tw_tabWidget)
         # -------------------------------------------------
         # -----------------hbl_nav_buttons_layout-----------------
@@ -53,8 +57,11 @@ class GCTabView(QFrame):
         self.btn_back = QPushButton(text="Back")
         self.btn_continue = QPushButton(text="Continue")
 
+        self.btn_back.clicked.connect(self.back_clicked)
+        self.btn_continue.clicked.connect(self.continue_clicked)
+
         self.hbl_nav_buttons_layout.addWidget(self.btn_back, alignment=QtCore.Qt.AlignRight)
-        self.hbl_nav_buttons_layout.addWidget(self.btn_continue)
+        self.hbl_nav_buttons_layout.addWidget(self.btn_continue, alignment=QtCore.Qt.AlignRight)
 
         # -------------------------------------------------
         # -----------------vbl_main_layout-----------------
@@ -68,6 +75,36 @@ class GCTabView(QFrame):
         self.setLayout(self.vbl_main_layout)
 
         # -------------------------------------------init End-------------------------------------------
+
+    def set_tab_index(self, index_num=0):
+        current_index = self.get_tab_index()
+        self.tw_tabWidget.setTabEnabled(current_index, False)
+
+        self.tw_tabWidget.setCurrentIndex(index_num)
+        self.tw_tabWidget.setTabEnabled(index_num, True)
+
+    def get_tab_index(self):
+        return self.tw_tabWidget.currentIndex()
+
+    def continue_clicked(self):
+        tab_index = self.get_tab_index()
+        if tab_index == 0:
+            self.set_tab_index(1)
+        elif tab_index == 1:
+            self.set_tab_index(2)
+        elif tab_index == 2:
+            pass
+
+    def back_clicked(self):
+        tab_index = self.get_tab_index()
+        if tab_index == 0:
+            pass
+        elif tab_index == 1:
+            self.set_tab_index(0)
+        elif tab_index == 2:
+            self.set_tab_index(1)
+
+
 
 if __name__ == '__main__':
     qApp = QApplication(sys.argv)
